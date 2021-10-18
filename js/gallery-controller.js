aa46c3b0ad9fd7c4679f77742bf3c09b5c8f4892
Mor-Meir <123agmor@gmail.com>
2021-10-18 17:14:18 +0300

    second commit

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,10 +1,20 @@
 'use strict'
 
+function onSubmitContact() {
+    // ev.preventdefault();
+    const email = $('#formControlEmail').val()
+    console.log('email', email);
+    const subject = $('#formControlSubject').val()
+    console.log('subject', subject);
+    const message = $('#formControlTextarea').val()
+    console.log('message', message);
+    const URL = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${message}`
+    window.open(URL, '_blank');
+
+}
+
 function onShowModal(projId) {
-    var $elModal = $('.modal-body');
-    console.log('$elModal', $elModal);
     var elProj = getProjById(projId);
-    // console.log('elProj', elProj);
     $('.modal-body h2').text(elProj.name)
     $('.modal-body .proj-title').text(elProj.title)
     var strHtml = `<img class="img-fluid d-block mx-auto" src="img/portfolio/02-full.jpg" alt="${elProj.name}">`;
@@ -21,11 +31,11 @@ function renderProjs() {
     var strHtmls = projs.map(proj => {
         return `<div class="col-md-4 col-sm-6 item" onclick="onShowModal('${proj.id}')">
         <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
-          <div class="portfolio-hover">
-            <div class="portfolio-hover-content">
-              <i class="fa fa-plus fa-3x"></i>
-            </div>
-          </div>
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content">
+          <i class="fa fa-plus fa-3x"></i>
+        </div>
+        </div>
           <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="${proj.name}">
         </a>
         <div class="portfolio-caption">
--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ console.log('Starting up');
 
 // $(document).ready(onInit);
 $(onInit);
-
+// $('.google-btn').on('click',  onSubmitContact)
+$('.google-btn').click(onSubmitContact)
 
 function onInit() {
     // $('.portfolio-item').click(onShowModal);
